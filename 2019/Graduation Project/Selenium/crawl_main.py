fc68615582ab86ec4f083b46cf5a65e89e98343e
JiMyungHwa <40465192+JiMyungHwa@users.noreply.github.com>
2019-09-21 00:44:04 +0900

    Update crawl_main.py

--- 2019/Graduation Project/Selenium/crawl_main.py
+++ 2019/Graduation Project/Selenium/crawl_main.py
@@ -1,8 +1,11 @@
 import sys
 import json
+from pprint import pprint
 import crawl_initiateChromeDriver
 import crawl_parseKeyword
-import crawl_parseSubKeyword
+import crawl_parseKoreanContents
+import crawl_parseEnglishContents
+import crawl_isEnglishOrKorean
 import crawl_parsePageList
 import crawl_parseRelativeKeyword
 import crawl_parseScreenshot
@@ -13,72 +16,87 @@ from datetime import datetime
 """
     프론트엔드에서 사용자가 키워드를 입력하거나 페이지를 옮기면서 url이 변할 경우 메인 함수에 json 객체가 들어옴(현재 페이지, 이전 페이지, 레벨, 태그)
     url을 응답해서 그 url에 맞는 크롤링 방식 채택
-    
-    몽고 DB에 들어갈 속성 : 사용자 번호, 프로젝트 방 번호, 키워드, 현재 페이지, 이전 페이지, 페이지 리스트, 레벨, 서브 키워드, 태그, 이미지, 추출 시간
+
+    몽고 DB에 들어갈 속성 : 사용자명, 이메일, 현재 페이지, 이전 페이지, 페이지 리스트, 레벨, 키워드, 서브 키워드, 본문 요약, 태그, 스크린 샷, 추출 시간
 """
 
-def main():
-
+def main(): # jsonFile):
     # 검색 페이지인 경우를 알기 위한 변수
     search_page = 'https://www.google.com/search?'
 
     # # 메인 함수에 들어오는 json 객체 파일 열기
-    # jsonFile = sys.argv[1]
     # with open(jsonFile) as f:
     #     data = json.loads(f.read())
     #
-    #     currUrl = data['currUrl']
-    #     prevUrl = data['prevUrl']
-    #     level = data['level']
-    #     tag = data['tag']
+    #     marked = data['marked']         # 해당 URL에 대해 마킹한 팀원 이메일
+    #     paths = data['paths']           # 호스트 주소를 포함한 현재 URL 경로
+    #     user_name = data['user_name']   # 노드를 등록한 팀원의 이름
+    #     user_email = data['user_email'] # 노드를 등록한 팀원의 이메일
+    #     parent_id = data['parent_id']   # 부모 노드의 ID
+    #     currUrl = data['currUrl']       # 사용자가 보고 있는 현재 페이지
+    #     prevUrl = data['prevUrl']       # 사용자가 봤었던 이전 페이지
+    #     level = data['level']           # 현재 페이지의 레벨
+    #     tag = data['tag']               # 사용자가 생각하는 우선 순위 태그
 
-    currUrl = sys.argv[1]  # 메인함수 첫 번째 인자로 현재 페이지 입력 받기
-    prevUrl = sys.argv[2] # 메인함수 두 번째 인자로 이전 페이지 입력 받기
-    level = sys.argv[3] # 메인함수 세 번째 인자로 레벨 입력 받기
-    tag = sys.argv[4] # 메인함수 네 번째 인자로 태그 입력 받기(태그 설정 유무)
+    user_name = sys.argv[1]
+    user_email = sys.argv[2]
+    currUrl = sys.argv[3]
+    prevUrl = sys.argv[4]
+    level = sys.argv[5]
+    tag = sys.argv[6]
 
     # 현재 페이지에 대한 크롤링 준비
     driver = crawl_initiateChromeDriver.initiateChromeDriver()
     driver.get(currUrl)
     driver.implicitly_wait(2)
 
-    # 페이지 본문 내용 추출을 위한 변수
-    pageContents = driver.find_element_by_xpath('/html/body')
-
-    # 만약 현재 페이지가 키워드를 입력하고 난 검색 페이지인 경우
-    # 키워드, url 리스트, 서브 키워드 추출
+    # 검색 페이지인 경우
     if search_page in currUrl:
-        print('===========검색 페이지인 경우===========')
-        keyword = crawl_parseKeyword.parseKeyword(driver)
+        print('검색 페이지인 경우')
+        user_name = user_name
+        user_email = user_email
+        keyword = crawl_parseKeyword.parseKeywordSearchPage(driver)
         currUrl = currUrl
         prevUrl = prevUrl
         pageList = crawl_parsePageList.parsePageList(driver)
         relativeKeywordList = crawl_parseRelativeKeyword.parseRelativeKeyword(driver)
+        pageContents = []
         subkeyword = []
-        imgs = crawl_parseScreenshot.parseScreenshot(driver, keyword + ".png")
         tags = tag
+        screenshot = crawl_parseScreenshot.parseScreenshot(driver, user_name, keyword)
         nowTime = datetime.now()
 
-    # 만약 현재 페이지가 홈페이지인 경우
-    # 서브 키워드 추출
+    # 홈페이지인 경우
     else:
-        print('============홈페이지인 경우============')
-        keyword = crawl_parseKeyword.parseKeyword(driver)
+        print('홈페이지인 경우')
+        user_name = user_name
+        user_email = user_email
+        keyword = crawl_parseKeyword.parseKeywordHomePage(driver)
         currUrl = currUrl
         prevUrl = prevUrl
         pageList = []
         relativeKeywordList = []
-        subkeyword = crawl_parseSubKeyword.parseSubKeyword(pageContents)
-        imgs = crawl_parseScreenshot.parseScreenshot(driver, keyword + ".png")
+
+        # 서브 키워드 및 본문 요약은 키워드가 한글 또는 영어에 따라 다르다.
+        if crawl_isEnglishOrKorean.isEnglishOrKorean(keyword) == 'kr':
+            textrank = crawl_parseKoreanContents.TextRank(currUrl)
+            pageContents = textrank.summarize(5)
+            subkeyword = textrank.keywords()
+        else:
+            textrank = crawl_parseEnglishContents.TextRank(currUrl)
+            pageContents = textrank.summarize(5)
+            subkeyword = textrank.keywords()
+
         tags = tag
+        screenshot = crawl_parseScreenshot.parseScreenshot(driver, user_name, keyword)
         nowTime = datetime.now()
 
     # 추출된 아이템들을 몽고 DB에 저장
-    crawl_saveMongoDB.store_mongoDB(keyword, currUrl, prevUrl, pageList, relativeKeywordList, level, subkeyword, imgs, tags, nowTime)
+    crawl_saveMongoDB.store_mongoDB(user_name, user_email, keyword, currUrl, prevUrl, pageList, relativeKeywordList, level, subkeyword, pageContents, screenshot, tags, nowTime)
 
     # 추출된 아이템들을 엑셀에 저장
-    crawl_saveExcel.make_excel()
-    crawl_saveExcel.crawl_saveExcel(keyword, currUrl, prevUrl, pageList, relativeKeywordList, level, subkeyword, tags, str(nowTime), imgs)
+    crawl_saveExcel.make_excel(user_name, keyword)
+    crawl_saveExcel.crawl_saveExcel(user_name, user_email, keyword, currUrl, prevUrl, pageList, relativeKeywordList, level, subkeyword, pageContents, screenshot, tags, str(nowTime))
 
     # 드라이버 연결 끊기
     driver.close()
