dc7d299fbbeaee0bb8cc3c4cd43a9d6da28ee3f2
JiMyungHwa <40465192+JiMyungHwa@users.noreply.github.com>
2019-09-21 00:44:27 +0900

    Update crawl_parseKeyword.py

--- 2019/Graduation Project/Selenium/crawl_parseKeyword.py
+++ 2019/Graduation Project/Selenium/crawl_parseKeyword.py
@@ -1,15 +1,20 @@
-import time
+import re
 
 # 키워드 추출 함수(검색창인 경우에만)
-def parseKeyword(driver):
-    print('키워드 추출 중')
-    for i in range(1, 40):
-        print('▶', end="", flush=True)
-        time.sleep(0.1)
-    print('')
+def parseKeywordSearchPage(driver):
+
+    crawl_keyword = driver.find_element_by_xpath('//*[@id="cdr_opt"]/div/div[2]/div[2]/div[3]/form/input[1]')
+    keyword = crawl_keyword.get_attribute('value')
+
+    return keyword
+
+# 키워드 추출 함수(홈페이지인 경우에만)
+def parseKeywordHomePage(driver):
 
     crawl_keyword = driver.find_element_by_xpath('/html/head/title')
     keyword = crawl_keyword.get_attribute('text')
-    split_keyword = keyword.split('-')
 
-    return split_keyword[0]
+    # 키워드 내에 특수문자 제거
+    keyword = re.sub('[\/]', '', keyword)
+
+    return keyword
