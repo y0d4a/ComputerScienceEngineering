5d4ad25cda71e98b2dc6aa4d22193b950712c05c
JiMyungHwa <40465192+JiMyungHwa@users.noreply.github.com>
2019-09-21 00:45:00 +0900

    Update crawl_parsePageList.py

--- 2019/Graduation Project/Selenium/crawl_parsePageList.py
+++ 2019/Graduation Project/Selenium/crawl_parsePageList.py
@@ -1,17 +1,18 @@
-import time
+from selenium.common.exceptions import NoSuchElementException
 
 # 페이지 리스트 추출 함수(검색창인 경우에만)
 def parsePageList(driver):
-    print('페이지 리스트 추출 중')
-    for i in range(1, 40):
-        print('▶', end="", flush=True)
-        time.sleep(0.1)
-    print('')
 
     pList = []
-    pageList = driver.find_element_by_xpath("//div[@id='rso']")
-    for p in pageList.find_elements_by_tag_name('cite'):
-        p = p.text
-        pList.append(p)
+
+    try:
+        pageList = driver.find_element_by_xpath("//div[@id='rso']")
+
+        for p in pageList.find_elements_by_tag_name('cite'):
+            p = p.text
+            pList.append(p)
+
+    except NoSuchElementException:
+        print("페이지 리스트가 페이지 내에 없습니다")
 
     return pList
