fce5c9af242d5275497fb1b62987bfde07c200fb
JiMyungHwa <40465192+JiMyungHwa@users.noreply.github.com>
2019-09-21 00:45:27 +0900

    Update crawl_parseRelativeKeyword.py

--- 2019/Graduation Project/Selenium/crawl_parseRelativeKeyword.py
+++ 2019/Graduation Project/Selenium/crawl_parseRelativeKeyword.py
@@ -1,19 +1,20 @@
-import time
+from selenium.common.exceptions import NoSuchElementException
 
 # 연관 키워드, url 추출 함수(검색창인 경우에만)
 def parseRelativeKeyword(driver):
-    print('연관 키워드 및 URL 추출 중')
-    for i in range(1, 40):
-        print('▶', end="", flush=True)
-        time.sleep(0.1)
-    print('')
 
     relativeKeyword = {}
-    relativeKeywordList = driver.find_element_by_xpath("//div[@id='brs']")
-    for r in relativeKeywordList.find_elements_by_tag_name('a'):
-        text = r.get_attribute('text')
-        href = r.get_attribute('href')
 
-        relativeKeyword[text] = href
+    try:
+        relativeKeywordList = driver.find_element_by_xpath("//div[@id='brs']")
+
+        for r in relativeKeywordList.find_elements_by_tag_name('a'):
+            text = r.get_attribute('text')
+            href = r.get_attribute('href')
+
+            relativeKeyword[text] = href
+
+    except NoSuchElementException:
+        print("연관 키워드가 페이지 내에 없습니다")
 
     return relativeKeyword
