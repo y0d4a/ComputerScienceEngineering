0677a5d37e14280deb95a1acc3d97440ec37e5bc
JMH <battlesun99@gamil.com>
2019-09-23 03:13:28 +0900

    commit

--- 2019/Graduation Project/Selenium/crawl_saveExcel.py
+++ 2019/Graduation Project/Selenium/crawl_saveExcel.py
@@ -1,5 +1,4 @@
 import datetime
-from pprint import pprint
 import openpyxl
 import re
 from openpyxl import Workbook, load_workbook
@@ -23,19 +22,21 @@ def make_excel(user_name, keyword):
     sheet1.title = excel_sheet_title
 
     # 헤더 입력
-    sheet1.cell(row=1, column=1).value = '사용자명'
-    sheet1.cell(row=1, column=2).value = '이메일'
-    sheet1.cell(row=1, column=3).value = '현재 페이지'
-    sheet1.cell(row=1, column=4).value = '이전 페이지'
-    sheet1.cell(row=1, column=5).value = '페이지 리스트'
-    sheet1.cell(row=1, column=6).value = '연관 검색어 리스트'
-    sheet1.cell(row=1, column=7).value = '레벨'
-    sheet1.cell(row=1, column=8).value = '키워드'
-    sheet1.cell(row=1, column=9).value = '서브 키워드'
-    sheet1.cell(row=1, column=10).value = '본문 요약'
-    sheet1.cell(row=1, column=11).value = '태그'
-    sheet1.cell(row=1, column=12).value = '추출 시간'
-    sheet1.cell(row=1, column=13).value = '스크린 샷'
+    sheet1.cell(row=1, column=1).value = 'user_name'
+    sheet1.cell(row=1, column=2).value = 'user_mail'
+    sheet1.cell(row=1, column=3).value = 'curr_url'
+    sheet1.cell(row=1, column=4).value = 'prev_url'
+    sheet1.cell(row=1, column=5).value = 'pageList'
+    sheet1.cell(row=1, column=6).value = 'relativeKeywordList'
+    sheet1.cell(row=1, column=7).value = 'level'
+    sheet1.cell(row=1, column=8).value = 'paths'
+    sheet1.cell(row=1, column=9).value = 'keyword'
+    sheet1.cell(row=1, column=10).value = 'sub_keyword'
+    sheet1.cell(row=1, column=11).value = 'pageContents'
+    sheet1.cell(row=1, column=12).value = 'memo'
+    sheet1.cell(row=1, column=13).value = 'tags'
+    sheet1.cell(row=1, column=14).value = 'nowTime'
+    sheet1.cell(row=1, column=15).value = 'screenshot'
 
     work_book.save(filename=excel_file_name)
     work_book.close()
@@ -60,7 +61,7 @@ def adjust_column_width(ws):
         ws.column_dimensions[col_name].width = adjust_width
 
 # 엑셀에 크롤링된 데이터 저장 함수
-def crawl_saveExcel(user_name, user_email, keyword, currUrl, prevUrl, pageList, relativeKeywordList, level, subkeyword, pageContents, screenshot, tags, nowTime):
+def crawl_saveExcel(user_name, user_email, curr_url, prev_url, pageList, relativeKeywordList, level, paths, keyword, sub_keyword, pageContents, memo, screenshot, tagged, nowTime):
 
     # 현재 날짜
     now = datetime.datetime.now()
@@ -80,28 +81,30 @@ def crawl_saveExcel(user_name, user_email, keyword, currUrl, prevUrl, pageList, 
     excel_column = 1
 
     crawl_info = {
-        "사용자명": user_name,
-        "이메일": user_email,
-        "현재 페이지": currUrl,
-        "이전 페이지": prevUrl,
-        "페이지 리스트": pageList,
-        "연관 검색어 : URL 리스트": relativeKeywordList,
-        "레벨": int(level),
-        "키워드": keyword,
-        "서브 키워드": subkeyword,
-        "본문 요약": pageContents,
-        "태그": tags,
-        "추출 시간": nowTime,
-        "스크린 샷": screenshot,
+        "user_name": user_name,
+        "user_email": user_email,
+        "curr_url": curr_url,
+        "prev_url": prev_url,
+        "pageList": pageList,
+        "relativeKeywordList": relativeKeywordList,
+        "level": int(level),
+        "paths": paths,
+        "keyword": keyword,
+        "sub_keyword": sub_keyword,
+        "pageContents": pageContents,
+        "memo": memo,
+        "tagged": tagged,
+        "nowTime": nowTime,
+        "screenshot": screenshot,
     }
 
     crawl_info_values = list(crawl_info.values())
 
     for idx, info_values in enumerate(crawl_info_values):
-        # 페이지 리스트(4), 서브 키워드(8), 본문 요약(9)인 경우(리스트 형태)
-        # 페이지 리스트(4)와 서브 키워드(8), 본문 요약(9)의 경우는 검색 페이지와 홈페이지인 경우 추출이 안될 수 있기 때문에
+        # 페이지 리스트(4), 서브 키워드(9), 본문 요약(10)인 경우(리스트 형태)
+        # 페이지 리스트(4)와 서브 키워드(9), 본문 요약(10)의 경우는 검색 페이지와 홈페이지인 경우 추출이 안될 수 있기 때문에
         # 빈 리스트를 고려해주어야 한다.
-        if idx == 4 or idx == 8 or idx == 9:
+        if idx == 4 or idx == 9 or idx == 10:
             if info_values == []:
                 sheet1.cell(row=excel_row, column=excel_column).value = ''
             else:
@@ -109,8 +112,8 @@ def crawl_saveExcel(user_name, user_email, keyword, currUrl, prevUrl, pageList, 
                     sheet1.cell(row=excel_row, column=excel_column).value = info
                     excel_row += 1
 
-        # 연관 검색어 : URL 리스트인 경우(딕셔너리 형태)(5)
-        # 연관 검색어 : URL 리스트(5) 역시 검색 페이지에서만 추출되기 때문에 홈페이지인 경우를 고려해 빈 리스트를 고려해주어야 한다.
+        # 연관 검색어 URL 리스트인 경우(딕셔너리 형태)(5)
+        # 연관 검색어 URL 리스트(5) 역시 검색 페이지에서만 추출되기 때문에 홈페이지인 경우를 고려해 빈 리스트를 고려해주어야 한다.
         elif idx == 5:
             if info_values == []:
                 sheet1.cell(row=excel_row, column=excel_column).value = ''
@@ -127,13 +130,13 @@ def crawl_saveExcel(user_name, user_email, keyword, currUrl, prevUrl, pageList, 
         elif idx == 6:
             sheet1.cell(row=excel_row, column=excel_column).value = info_values
 
-        # 스크린 샷인 경우(문자열 형태)
-        elif idx == 12:
+        # 스크린 샷(14)인 경우(문자열 형태)
+        elif idx == 14:
             png_loc = 'C:/Users/battl/PycharmProjects/myProject02/screenshot/' + user_name + '_' + keyword + '.png'
             png = openpyxl.drawing.image.Image(png_loc)
-            sheet1.add_image(png, 'M2')
+            sheet1.add_image(png, 'P2')
 
-        # 사용자명, 이메일, 현재 페이지, 이전 페이지, 키워드, 태그, 추출 시간인 경우(문자열 형태)
+        # 사용자명, 이메일, 현재 페이지, 이전 페이지, 경로, 키워드, 메모, 태그, 추출 시간인 경우(문자열 형태)
         else:
             if info_values == []:
                 sheet1.cell(row=excel_row, column=excel_column).value = ''
