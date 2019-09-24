79860792b5210a01526332c20505308ec15f0708
JMH <battlesun99@gamil.com>
2019-09-24 21:27:06 +0900

    commit

--- 2019/Graduation Project/Selenium/Function7.py
+++ 2019/Graduation Project/Selenium/Function7.py
@@ -5,27 +5,11 @@
     2) 키워드 중심으로 키워드 분포도, 워드 클라우드 등 데이터 시각화 모델 생성
 """
 
-import matplotlib.pyplot as plt
-from matplotlib import font_manager, rc
-from matplotlib import style
-from matplotlib import colors as mcolors
 from pymongo import MongoClient
-import pandas as pd
 
-# 몽고 DB 연결
-conn = MongoClient('mongodb+srv://dots_user:TzE66c5O0KB0bnjG@dots-test-x41en.mongodb.net/test?retryWrites=true&w=majority')
-db = conn['JMH']
-collection = db['second_integrated_user_table']
+class Function7:
 
-# 폰트 설정
-font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
-rc('font', family=font_name)
-style.use('ggplot')
-
-# 데이터 시각화
-colors = dict(mcolors.BASE_COLORS, **mcolors.CSS4_COLORS)
-plt.rcParams["figure.figsize"] = (10,4)     # 그림 크기
-plt.rcParams['lines.linewidth'] = 2         # 선의 두께
-plt.rcParams['axes.grid'] = True            # 차트 내 격자선 표시 여부
-plt.pie(table['freq'], labels=table['curr_url'], colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
-plt.show()
+    def __init__(self):
+        conn = MongoClient('mongodb+srv://dots_user:TzE66c5O0KB0bnjG@dots-test-x41en.mongodb.net/test?retryWrites=true&w=majority')
+        db = conn['JMH']
+        collection = db['second_integrated_user_table']
